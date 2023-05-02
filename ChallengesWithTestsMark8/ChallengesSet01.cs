19ea08c2a91a0709286cf4855af75a6c6cd70f6c
alica kavuljakova <drobeek@gmail.com>
2023-05-02 07:22:19 -0700

    homework done

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -4,6 +4,7 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
+        
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
@@ -17,33 +18,48 @@ namespace ChallengesWithTestsMark8
         }
 
         public double Subtract(double minuend, double subtrahend)
-        {
-            throw new NotImplementedException();
+        { 
+            return minuend - subtrahend;
+            
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+           return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 <= number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }      
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+               return "Hello, " + nameOfPerson;
+            }
+            else
+            {
+                return "Hello";
+            }
+           // return string.IsNullOrEmpty(nameOfPerson) ? "Hello" : $"Hello,{nameOfPerson}";
         }
 
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
+        public string GetHey() => "HEY";
+       
+      
     }
 }
